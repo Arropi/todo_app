f6f3299ff2a4da7104102314de70b34141f36910
arrofi <m.faizarrofi@gmail.com>
2025-01-31 21:25:28 +0700

    Sedikit Penambahan Filter dan Grouping

--- connection.js
+++ connection.js
@@ -1,11 +1,7 @@
 const { Client } = require('pg')
 
 const db = new Client ({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "123456",
-    database: "gdgoc_todos",
+    connectionString: process.env.DATABASE_URL
 })
 
 module.exports = db
